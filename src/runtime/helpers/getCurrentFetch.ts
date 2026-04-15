da0faadf08c9543d696f535869e718ff141c39dd
Eliott DEPAUW <e.depauw@xefi.fr>
2026-04-15 16:35:20 +0200

    change version

--- src/runtime/helpers/getCurrentFetch.ts
+++ src/runtime/helpers/getCurrentFetch.ts
@@ -2,6 +2,5 @@ import { useNuxtApp } from 'nuxt/app'
 
 export default () => {
   const { $laravelRaom } = useNuxtApp()
-  console.log('getCurrentFetch: $laravelRaom.fetch =', $laravelRaom?.fetch)
   return ($laravelRaom as { fetch: typeof $fetch })?.fetch as typeof $fetch ?? $fetch
 }
