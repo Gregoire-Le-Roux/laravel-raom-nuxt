9c911290b398091d6119910a08dfbea983342ff1
Grégoire <84314581+Gregoire-Le-Roux@users.noreply.github.com>
2026-04-30 10:05:33 +0200

    Update QueryBuilder.ts to add trim on text search
    Laravel do a trim on the textSearch but when textSearch is equal to " ", the query become invalid and cause an error 500, so before setting the search text we check if the value trimmed is valid

--- src/runtime/query/QueryBuilder.ts
+++ src/runtime/query/QueryBuilder.ts
@@ -325,7 +325,7 @@ export class QueryBuilder<T extends Model> {
       search: {},
     }
 
-    if (this.textSearch) {
+    if (this.textSearch?.trim()) {
       payload.search.text = { value: this.textSearch }
     }
 
